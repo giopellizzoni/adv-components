19ea4a61b2d6e46e7c8fd802420bdb174a33babe
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-02-13 17:16:07 +0100

    add Form component and update App to include input fields for name and age

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Input from "./components/Input";
+import Form from "./components/Form";
 import Button from "./components/Button";
-import Container from "./components/Container";
 
 function App() {
   return (
     <main>
-      <Container as={Button}>Click me</Container>
+      <Form>
+        <Input label="Name" id="name" type="text" />
+        <Input label="Age" id="age" type="number" />
+        <p>
+          <Button>Submit</Button>
+        </p>
+      </Form>
     </main>
   );
 }
