4fc6d698eb1e143e236a5715103b074708cd0e4b
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-02-13 18:15:41 +0100

    add AddTimer and Header components; refactor App to include new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import Input from "./components/UI/Input";
-import Form, { type FormHandle } from "./components/UI/Form";
-import Button from "./components/UI/Button";
-import { useRef } from "react";
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
 
 function App() {
-  const customForm = useRef<FormHandle>(null);
-
-  function handleSave(data: unknown) {
-    const extractedData = data as { name: string; age: number };
-
-    console.log(extractedData);
-
-    customForm.current?.clear();
-  }
-
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input label="Name" id="name" type="text" />
-        <Input label="Age" id="age" type="number" />
-        <p>
-          <Button>Submit</Button>
-        </p>
-      </Form>
-    </main>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
   );
 }
 
