eeb384f8cc86fa69910e7e58c6f1feec6f411f53
Giovanni Pellizzoni <pellizzonicode@gmail.com>
2025-02-13 16:58:49 +0100

    initialize Vite + React + TypeScript project with basic components and configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from "./components/Button";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <main>
+      <Container as={Button}>Click me</Container>
+    </main>
+  );
+}
+
+export default App;
